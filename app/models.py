892792e8f62671303f6b4e8ff700f2b135469c9c
Pedro Goulart Branco <72553813+PedroGoulartbranco@users.noreply.github.com>
2025-12-16 11:25:56 -0300

    a

--- app/models.py
+++ app/models.py
@@ -6,3 +6,15 @@ class Usuario(db.Model):
     senha = db.Column(db.String(200), nullable=False)
     dinheiro = db.Column(db.Integer, nullable=False)
     cliques = db.Column(db.Integer, default=0)
+
+class Itens(db.Model):
+    id_item = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), unique=True, nullable=False)
+    descricao = db.Column(db.String(200), unique=True, nullable=False)
+    preco = db.Column(db.Float, nullable=False)
+
+class Inventario(db.Model):
+    id_inventario =  db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('itens.id_item'), nullable=False)
+    quantidade = db.Column(db.Integer, default=1)
